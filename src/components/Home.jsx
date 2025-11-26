8f9f2b9b909d7f16e8db73f275d1d6932023dc47
Yusuf Göçen <128933870+YusufGocen@users.noreply.github.com>
2025-11-26 16:56:47 +0300

    Fix: nameContainer Fixed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
-import {motion} from 'framer-motion'
 import { FaLinkedin } from "react-icons/fa6";
 import { FaGithub } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { dark, vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import {  vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 export const Home = () => {
   return (
-    <motion.div className='home' initial={{opacity:0}} animate={{opacity:1}} transition={{duration:0.8 , delay:0.2}}>
+    <div className='home' initial={{opacity:0}} animate={{opacity:1}} transition={{duration:0.8 , delay:0.2}}>
       <div className='container'>
         <p id='Home'>Hello, I'm</p>
-        <h1>Yusuf Göçen</h1>
-        <h3 style={{paddingTop:"0.5rem"}}>Connect With Me</h3>
+        <h1 style={{letterSpacing:10}}>Yusuf Göçen</h1>
+        <h2>Frontend / Mobile Developer</h2>
+        <h4 style={{paddingTop:"0.5rem"}}>Connect With Me</h4>
         <div className='links'>
         <a target="_newblank" href="https://www.linkedin.com/in/yusufgocen/">
           <FaLinkedin />
@@ -31,8 +31,8 @@ export const Home = () => {
             <h1>About Me</h1>
           </div>  
         <div className='Code'>
-          <SyntaxHighlighter language='typescript' style={vscDarkPlus} className='SyntaxCode' customStyle={{margin:0, padding:"2rem", height:"100%", borderRadius:"20px",width:"50rem"
-            ,backdropFilter:"blur(10px)",marginBottom:20 , background:"rgba(22, 22, 25, 0.85)"}}>
+          <SyntaxHighlighter language='typescript' style={vscDarkPlus} className='SyntaxCode' customStyle={{margin:0, padding:"2rem", height:"100%", borderRadius:"20px",  width: "100%",
+            maxWidth: "50rem",overflowX: "auto",backdropFilter:"blur(10px)",marginBottom:20 , background:"rgba(22, 22, 25, 0.85)"}}>
             { `const AboutMe : DeveloperProfile={
     Name:"Yusuf Göçen",
     title: "Frontend / Mobile Developer",
@@ -51,10 +51,16 @@ education: {
    },
 experience: [
  {  
+    company: "Rast Mobile",
+    role: "Software Developer Intern",
+    location: "İstanbul",
+    duration: "Oct 2025 - Present"
+  },
+ {  
     company: "Flo Group",
     role: "Intern",
     location: "İstanbul",
-    duration: "Aug 2025 (1 Month)"
+    duration: "Aug 2025"
   },
   {
     company: "Smartiks Technology Solutions",
@@ -64,13 +70,13 @@ experience: [
   }       
   ],
 stack: {
-  languages: ["JavaScript", "TypeScript", "Java", "SQL"],
-  technologies: ["HTML", "CSS (Tailwind, Bootstrap)", "React.js", "React Native", "Spring Boot", "Git"]
+  languages: ["JavaScript", "TypeScript", "Java", "Python" , "SQL"],
+  technologies: ["HTML","CSS(Tailwind, Bootstrap)","React.js","React Native","Next.js","Redux","Spring Boot","Git"]
   },`}
             
           </SyntaxHighlighter>
         </div>
       </div>
-    </motion.div>
+    </div>
   )
 }
